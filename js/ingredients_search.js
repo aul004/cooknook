39a60996d5dae517fd0cfb83d6c55b7509c3b14a
Audrey Lee <aul004@ucsd.edu>
2017-08-31 11:32:23 -0700

    encapsulate the stage fns

--- js/ingredients_search.js
+++ js/ingredients_search.js
@@ -24,77 +24,81 @@ var ingredients_search_col_2;
 var ingredients_search_col_3;
 var ingredients_search_page;
 var queue = true;
-// *ingredients + *search results + recipe
-function stageD() {
-	currentStage = ingredients_search_stage.stage3;
-	$(ingredients_search_col_3.parentElement).css("width", currentStage.col3);
 
-	$(ingredients_search_col_1.parentElement).stop().animate({
-		width: currentStage.col1
-	}, { duration: 300, queue: queue });
+var ingredients_search_stage_functions = {
+	// *ingredients + *search results + recipe
+	stageD: function () {
+		currentStage = ingredients_search_stage.stage3;
+		$(ingredients_search_col_3.parentElement).css("width", currentStage.col3);
 
-	$(ingredients_search_col_2.parentElement).stop().animate({
-		width: currentStage.col2
-	}, { duration: 300, queue: queue });
+		$(ingredients_search_col_1.parentElement).stop().animate({
+			width: currentStage.col1
+		}, { duration: 300, queue: queue });
 
-	$(ingredients_search_col_1).click(function(){});
-	$(ingredients_search_col_2).click(stageC);
-	$(ingredients_search_col_3).click(stageC);
-}
+		$(ingredients_search_col_2.parentElement).stop().animate({
+			width: currentStage.col2
+		}, { duration: 300, queue: queue });
 
-// ingredients + *search results + *recipe
-function stageC() {
-	currentStage = ingredients_search_stage.stage2;
-	$(ingredients_search_col_1.parentElement).stop().animate({
-		width: currentStage.col1
-	}, { duration: 200, queue: queue });
-	$(ingredients_search_col_2.parentElement).stop().animate({
-		width: currentStage.col2
-	}, { duration: 200, queue: queue });
+		$(ingredients_search_col_1).click(function(){});
+		$(ingredients_search_col_2).click(ingredients_search_stage_functions.stageC);
+		$(ingredients_search_col_3).click(ingredients_search_stage_functions.stageC);
+	},
 
-	if(ingredients_search_col_3 == undefined) {
-		ingredients_search_col_3 = generateColumns();
-		ingredients_search_page.append(ingredients_search_col_3.parentElement);
-		changeTheme(currentTheme);;
+	// ingredients + *search results + *recipe
+	stageC: function() {
+		currentStage = ingredients_search_stage.stage2;
+		$(ingredients_search_col_1.parentElement).stop().animate({
+			width: currentStage.col1
+		}, { duration: 200, queue: queue });
+		$(ingredients_search_col_2.parentElement).stop().animate({
+			width: currentStage.col2
+		}, { duration: 200, queue: queue });
+
+		if(ingredients_search_col_3 == undefined) {
+			ingredients_search_col_3 = generateColumns();
+			ingredients_search_page.append(ingredients_search_col_3.parentElement);
+			changeTheme(currentTheme);;
+		}
+
+		$(ingredients_search_col_3.parentElement).stop().animate({
+			width: currentStage.col3
+		}, { duration: 300, queue: queue });
+
+		$(ingredients_search_col_1).click(ingredients_search_stage_functions.stageD);
+		$(ingredients_search_col_2).click(function(){});
+		$(ingredients_search_col_3).click(function(){});
+	},
+
+	// *ingredients + *search results
+	stageB: function() {
+		currentStage = ingredients_search_stage.stage1;
+		$(ingredients_search_col_1.parentElement).stop().animate({
+			width: currentStage.col1
+		}, { duration: 200, queue: queue });
+
+		if(ingredients_search_col_2 == undefined){
+			ingredients_search_col_2 = generateColumns();
+			ingredients_search_page.append(ingredients_search_col_2.parentElement);
+			changeTheme(currentTheme);
+		}
+		$(ingredients_search_col_2.parentElement).css("width", currentStage.col2);
+		$(ingredients_search_col_1).click(ingredients_search_stage_functions.stageB);
+		$(ingredients_search_col_2).click(ingredients_search_stage_functions.stageC);
+	},
+
+	// ingredients search splash
+	stageA: function() {
+		currentStage = ingredients_search_stage.stage0;
+		if(ingredients_search_col_1 == undefined) {
+			ingredients_search_col_1 = generateColumns();
+			ingredients_search_page.append(ingredients_search_col_1.parentElement);
+			populate_col_1();
+			changeTheme(currentTheme);
+		}
+		$(ingredients_search_col_1.parentElement).css("width", currentStage.col1);
+		$(ingredients_search_col_1).click(ingredients_search_stage_functions.stageB);
 	}
 
-	$(ingredients_search_col_3.parentElement).stop().animate({
-		width: currentStage.col3
-	}, { duration: 300, queue: queue });
-
-	$(ingredients_search_col_1).click(stageD);
-	$(ingredients_search_col_2).click(function(){});
-	$(ingredients_search_col_3).click(function(){});
-}
-
-// *ingredients + *search results
-function stageB() {
-	currentStage = ingredients_search_stage.stage1;
-	$(ingredients_search_col_1.parentElement).stop().animate({
-		width: currentStage.col1
-	}, { duration: 200, queue: queue });
-
-	if(ingredients_search_col_2 == undefined){
-		ingredients_search_col_2 = generateColumns();
-		ingredients_search_page.append(ingredients_search_col_2.parentElement);
-		changeTheme(currentTheme);
-	}
-	$(ingredients_search_col_2.parentElement).css("width", currentStage.col2);
-	$(ingredients_search_col_1).click(stageB);
-	$(ingredients_search_col_2).click(stageC);
-}
-
-// ingredients search splash
-function stageA() {
-	currentStage = ingredients_search_stage.stage0;
-	if(ingredients_search_col_1 == undefined) {
-		ingredients_search_col_1 = generateColumns();
-		ingredients_search_page.append(ingredients_search_col_1.parentElement);
-		populate_col_1();
-		changeTheme(currentTheme);
-	}
-	$(ingredients_search_col_1.parentElement).css("width", currentStage.col1);
-	$(ingredients_search_col_1).click(stageB);
 }
 
 // will rename this later >__>
@@ -115,7 +119,7 @@ function ingredients_page_setup() {
 		ingredients_search_page = document.createElement('div');
 		ingredients_search_page.id = 'ingredients_search_page';
 		document.getElementById('page_viewer').append(ingredients_search_page);
-		stageA();
+		ingredients_search_stage_functions.stageA();
 	} 
 	$(ingredients_search_page).css("display", "block");
 }
