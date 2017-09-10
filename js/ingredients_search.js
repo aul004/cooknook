ee1b112512b4fb15a9327f7e684c1b056dceba6e
Audrey Lee <aul004@ucsd.edu>
2017-09-09 18:48:13 -0700

    made it more functional

--- js/ingredients_search.js
+++ js/ingredients_search.js
@@ -22,6 +22,12 @@ var currentStage = ingredients_search_stage.stage0;
 var ingredients_search_col_1; 
 var ingredients_search_col_2;
 var ingredients_search_col_3;
+
+var ingredients_col1_ABD; 
+var ingredients_col1_C;
+var ingredients_col2_BCD;
+var ingredients_col3_CD;
+
 var ingredients_search_page;
 var queue = true;
 
@@ -29,19 +35,20 @@ var ingredients_search_stage_functions = {
 	// *ingredients + *search results + recipe
 	stageD: function () {
 		currentStage = ingredients_search_stage.stage3;
+
 		$(ingredients_search_col_3.parentElement).css("width", currentStage.col3);
 
 		$(ingredients_search_col_1.parentElement).stop().animate({
 			width: currentStage.col1
 		}, { duration: 300, queue: queue });
-
+		ingredients_populate_col_1();
 		$(ingredients_search_col_2.parentElement).stop().animate({
 			width: currentStage.col2
 		}, { duration: 300, queue: queue });
 
 		$(ingredients_search_col_1).click(function(){});
 		$(ingredients_search_col_2).click(ingredients_search_stage_functions.stageC);
-		$(ingredients_search_col_3).click(ingredients_search_stage_functions.stageC);
+		$(ingredients_search_col_3).click(function(){});
 	},
 
 	// ingredients + *search results + *recipe
@@ -50,6 +57,7 @@ var ingredients_search_stage_functions = {
 		$(ingredients_search_col_1.parentElement).stop().animate({
 			width: currentStage.col1
 		}, { duration: 200, queue: queue });
+		ingredients_populate_col_1_hidden();	
 		$(ingredients_search_col_2.parentElement).stop().animate({
 			width: currentStage.col2
 		}, { duration: 200, queue: queue });
@@ -57,13 +65,7 @@ var ingredients_search_stage_functions = {
 		if(ingredients_search_col_3 == undefined) {
 			ingredients_search_col_3 = generateColumns();
 			ingredients_search_page.append(ingredients_search_col_3.parentElement);
-
-			var rresult = document.createElement("img");
-			rresult.setAttribute("src", "img/rresult.png");
-			rresult.setAttribute("id", "img2");
-
-			ingredients_search_col_3.appendChild(rresult);
-			changeTheme(currentTheme);;
+			ingredients_search_populate_col_3_recipe();
 		}
 
 		$(ingredients_search_col_3.parentElement).stop().animate({
@@ -72,9 +74,7 @@ var ingredients_search_stage_functions = {
 
 		$(ingredients_search_col_1).click(ingredients_search_stage_functions.stageD);
 		$(ingredients_search_col_2).click(function(){});
-		$(ingredients_search_col_3).click(function(){});
-		$("#img2").click(ingredients_search_stage_functions.stageB);
-		
+		$(ingredients_search_col_3).click(function(){});	
 	},
 
 	// *ingredients + *search results
@@ -83,79 +83,13 @@ var ingredients_search_stage_functions = {
 		$(ingredients_search_col_1.parentElement).stop().animate({
 			width: currentStage.col1
 		}, { duration: 200, queue: queue });
-
+		// ingredients_populate_col_1();
 		if(ingredients_search_col_2 == undefined){
 			ingredients_search_col_2 = generateColumns();
 			ingredients_search_page.append(ingredients_search_col_2.parentElement);
-			var title = document.createElement("h3");
-			ingredients_search_col_2.appendChild(title);
-			title.innerHTML = "You can make...";
-			
-
-			var ddbutton1 = document.createElement("div");
-			ddbutton1.setAttribute("class", "btn-group dropdown");
-			ingredients_search_col_2.appendChild(ddbutton1);
-			ddbutton1.innerHTML = "<button class=\"btn btn-secondary dropdown-toggle\" style=\"float:right;\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">" +
-			"Best Match</button>" +
-			"<div class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton\">" +
-			"<a class=\"dropdown-item\" href=\"#\">Cooking Time</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">Difficulty Level</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">Least Missing Ingredients</a>" +
-			"</div>";
-
-			var ddbutton2 = document.createElement("div");
-			ddbutton2.setAttribute("class", "btn-group dropdown");
-			ingredients_search_col_2.appendChild(ddbutton2);
-			ddbutton2.innerHTML = "<button class=\"btn btn-secondary dropdown-toggle\" style=\"float:right;\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">" +
-			"All Cuisines</button>" +
-			"<div class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton\">" +
-			"<a class=\"dropdown-item\" href=\"#\">Italian</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">German</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">Korean</a>" +
-			"</div>";
-
-			var ddbutton3 = document.createElement("div");
-			ddbutton3.setAttribute("class", "btn-group dropdown");
-			ingredients_search_col_2.appendChild(ddbutton3);
-			ddbutton3.innerHTML = "<button class=\"btn btn-secondary dropdown-toggle\" style=\"float:right;\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">" +
-			"All Types</button>" +
-			"<div class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton\">" +
-			"<a class=\"dropdown-item\" href=\"#\">Sandwich</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">Soup</a>" +
-			"<a class=\"dropdown-item\" href=\"#\">Salad</a>" + 
-			"<a class=\"dropdown-item\" href=\"#\">Dessert</a>" +
-			"</div>";
-			// var ddbutton2 = document.createElement("div");
-			// filterdiv.appendChild(ddbutton2);
-			// ddbutton2.innerHTML = "<button style=\"float:right;\" class=\"btn btn-primary dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\">Filter 1" + 
-			// 					  "<span class=\"caret\"></span></button>" + 
-			// 					  "<ul class=\"dropdown-menu\">" +
-			//   					  "<li><a href=\"#\">HTML</a></li>" +
-			//                       "<li><a href=\"#\">CSS</a></li>" +
-			//   					  "<li><a href=\"#\">JavaScript</a></li>" +
-			// 						"</ul>";
-
-			// var ddbutton3 = document.createElement("div");
-			// filterdiv.appendChild(ddbutton3);
-			// ddbutton3.innerHTML = "<button style=\"float:right;\" class=\"btn btn-primary dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\">Filter 1" + 
-			// 					  "<span class=\"caret\"></span></button>" + 
-			// 					  "<ul class=\"dropdown-menu\">" +
-			//   					  "<li><a href=\"#\">HTML</a></li>" +
-			//                       "<li><a href=\"#\">CSS</a></li>" +
-			//   					  "<li><a href=\"#\">JavaScript</a></li>" +
-			//   					  "</ul>";
-			//put populate col 2 here
-
-			var iresult = document.createElement("img");
-			iresult.setAttribute("src", "img/iresult.png");
-			iresult.setAttribute("id", "img1");
-
-			ingredients_search_col_2.appendChild(iresult);
-			changeTheme(currentTheme);
+			ingredients_search_populate_col_2_recipes();
 		}
 		$(ingredients_search_col_2.parentElement).css("width", currentStage.col2);
-		$(ingredients_search_col_1).click(ingredients_search_stage_functions.stageB);
-		$('#img1').click(ingredients_search_stage_functions.stageC);
 	},
 
 	// ingredients search splash
@@ -165,73 +99,223 @@ var ingredients_search_stage_functions = {
 			ingredients_search_col_1 = generateColumns();
 			ingredients_search_page.append(ingredients_search_col_1.parentElement);
 			ingredients_populate_col_1();
-			changeTheme(currentTheme);
 		}
 		$(ingredients_search_col_1.parentElement).css("width", currentStage.col1);
-		$('#addIngredient').click(ingredients_search_stage_functions.stageB);
 	}
 }
 
-// will rename this later >__>
-function ingredients_populate_col_1() {
-	var title = document.createElement("h3");
-	title.innerHTML = "What's in your pantry today?";
+function ingredients_search_populate_col_3_recipe() {
+	ingredients_search_col_3.innerHTML = '';
+	if(ingredients_col3_CD == undefined) {
+		ingredients_col3_CD = document.createElement('div');
+		var recipe = document.createElement("img");
+		recipe.src = "img/rresult.png";
+		recipe.id = 'img2';
+		ingredients_col3_CD.appendChild(recipe);
+	}
+	ingredients_search_col_3.appendChild(ingredients_col3_CD);
+}
 
-	var input = document.createElement("input");
-	var button = document.createElement('button');
-	var list = document.createElement('ul');
+function ingredients_search_populate_col_2_recipes() {
+    // social_friends_page_hide();
+    // social_profile_page_hide();
+    if(ingredients_col2_BCD == undefined) {
+        ingredients_col2_BCD = document.createElement('div');
 
-	input.style.height="50px";
-	input.style.width="60%";
-	input.style.fontSize="14pt";
+        var title = document.createElement("h3");
+        title.innerHTML = "You can make...";
+        ingredients_col2_BCD.appendChild(title);
 
-	title.style.paddingTop="10%";
-	list.style.paddingTop="2%";
+        var filterBar = document.createElement('div');
+        filterBar.className = "container";
+        ingredients_col2_BCD.appendChild(filterBar);
 
-	button.setAttribute("class", "searchbutton");
+        var filterOptions = {
+            'sort': ['Best Match', 'Cooking Time', 'Difficulty Level', 'Fewest Missing Ingredients'],
+            'cuisine': ['Italian', 'German', 'Korean'],
+            'type' : ['Appetizer', 'Soup', 'Salad', 'Entree']
+        }
 
-	ingredients_search_col_1.appendChild(title);
-	ingredients_search_col_1.appendChild(input);
-	// ingredients_search_col_1.appendChild(button);
+        for( var filterOption in filterOptions) {
+			var dropdownMenu = document.createElement('select');
+			ingredients_col2_BCD.appendChild(dropdownMenu);
 
-	input.style.marginTop="30px";
+			for (var option of filterOptions[filterOption]) {
+				var opt = document.createElement('option');
+				opt.value = option;
+				opt.innerHTML = option;
+				dropdownMenu.appendChild(opt);
+			}
+        }
 
-	var searchdiv = document.createElement("div");
-	searchdiv.setAttribute("class", "container");
-	searchdiv.style.marginTop="20px";
-	searchdiv.appendChild(button);
+        ingredients_col2_BCD.appendChild(document.createElement("hr"));
 
-	ingredients_search_col_1.appendChild(searchdiv);
+        var recipes = [
+            {
+                "name": "Wrap",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://verdecostamesa.com/wp-content/uploads/2017/03/ca-grill-food2-100x100.jpg"
+            },
+            {
+                "name": "Pizza",
+                "ratings": "img/cooknook_5star.png",
+                "src": "https://i.pinimg.com/736x/80/3a/ce/803ace870f3e6677d043883994c8d6e5--pizza-coupons-food-coupons.jpg"
+            },
+            {
+                "name": "Food",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://dining.savannahnow.com/sites/dining.savannahnow.com/files/styles/thumbnail/public/field/photos/2568845_web1_sav_052417_onfood-2.jpg"
+            },
+            {
+                "name": "Soup",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/thumb_small/public/posole.jpg"
+            },
+            {
+                "name": "Soba",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/thumb_small/public/41-papayasalad_1.jpg"
+            },
+            {
+                "name": "Zucchini Boats",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://www.topinspired.com/wp-content/uploads/2017/08/Greek-Quinoa-Grilled-Zucchini-Boats-100x100.jpg"
+            },
+            {
+                "name": "Fettuccine Alfredo",
+                "ratings": "img/cooknook_5star.png",
+                "src": "http://dining.savannahnow.com/sites/dining.savannahnow.com/files/styles/thumbnail/public/field/photos/2203750_web1_sav_042617_onfood-1.jpg"
+            }
 
-	input.id = 'ingredient';
-	button.id = 'addIngredient';
-	button.innerHTML = 'Add Ingredient +';
-	list.id = 'ingList';
+        ]
 
-	var addIng = function () {
-		var text = document.getElementById('ingredient').value;
-		if (text.length < 1)
-			return;
-		var li = document.createElement('div');
-		document.getElementById('ingList').appendChild(li);
-		document.getElementById("ingredient").value = '';
+        for (var recipe of recipes) {
+            var recipe_box = document.createElement('div');
+            recipe_box.className = 'container recipe-box text-left';
+            $(recipe_box).click(ingredients_search_stage_functions.stageC);
+            recipe_box.style = "width: 45%;  margin: 10px; background-color: white; float: left; padding: 10px 0px 10px 0px; border-radius: 7px; color: " + ingredients_search_theme.mainColor;
+            ingredients_col2_BCD.appendChild(recipe_box);
 
-		var label = document.createElement('label');
-		label.innerHTML = text;
-		li.appendChild(label);
+            var recipe_img_container = document.createElement('div');
+            recipe_img_container.className = 'text-center';
+            recipe_box.appendChild(recipe_img_container);
 
-		var deleteButton = document.createElement('button');
-		deleteButton.className = 'delete';
-		deleteButton.innerHTML = '-';
-		deleteButton.onclick = function(){
-			document.getElementById('ingList').removeChild(this.parentElement);
-		}
-		li.appendChild(deleteButton);
+            var recipe_img = document.createElement('img');
+            $(recipe_img).css('max-width', '100%');
+            recipe_img.src = recipe.src;
+            recipe_img.style = 'float:left; margin: 0px 20px 0px 10px;';
+            recipe_img.className = 'recipe_img';
+            recipe_img_container.appendChild(recipe_img);
+
+            var recipe_description = document.createElement('div');
+            recipe_description.style = 'float:left;';
+            recipe_box.appendChild(recipe_description);
+            
+            var recipe_name = document.createElement('h6');
+            recipe_name.style = 'color: ' + ingredients_search_theme.secondaryColor ;
+            recipe_name.innerHTML = recipe.name;
+            recipe_description.appendChild(recipe_name);
+
+            var ratings = document.createElement('img');
+            ratings.src = recipe.ratings;
+            $(ratings).css('max-width', '100px');
+            recipe_description.appendChild(ratings);
+
+            recipe_description.appendChild(document.createElement('br'));
+
+            var btn = document.createElement('img');
+            $(btn).css('max-width', '100%');
+            btn.src = 'img/ingr_buttons.jpg';
+            recipe_description.appendChild(btn);
+
+
+        }
+    }
+    else {
+        $(ingredients_col2_BCD).css("display", "block");
+
+        $('.recipe-box').css('width', '45%');
+        $('.recipe-box').addClass('text-left');
+        $('.recipe-box').removeClass('text-center');
+
+        $('.recipe_img').css('float', 'left');
+        $('.recipe_img').css('margin', '0px 20px 0px 10px;');
+	}
+	
+	ingredients_search_col_2.appendChild(ingredients_col2_BCD);
+}
+
+function ingredients_populate_col_1_hidden() {
+	if(ingredients_col1_ABD != undefined && ingredients_col1_ABD.parentElement == ingredients_search_col_1)
+		ingredients_search_col_1.removeChild(ingredients_col1_ABD);
+	if(ingredients_col1_C == undefined) {
+		ingredients_col1_C = document.createElement('div');
+		ingredients_col1_C.className = 'row'
+		
+		var expandBtn = document.createElement('div');
+		expandBtn.innerHTML = '+';
+		expandBtn.className = 'btn col';
+		ingredients_col1_C.appendChild(expandBtn);
 	}
 
-	document.getElementById('addIngredient').onclick = addIng;
+	ingredients_search_col_1.appendChild(ingredients_col1_C);
+}
+function ingredients_populate_col_1() {
+	if(ingredients_col1_C != undefined && ingredients_col1_C.parentElement == ingredients_search_col_1)
+		ingredients_search_col_1.removeChild(ingredients_col1_C);
+	if(ingredients_col1_ABD == undefined) {
+		ingredients_col1_ABD = document.createElement('div');
 
-	ingredients_search_col_1.appendChild(list);
+		var title = document.createElement("h3");
+		title.innerHTML = "What's in your pantry today?";
+		title.style.paddingTop="10%";
+		ingredients_col1_ABD.appendChild(title);
+		
+		var input = document.createElement("input");
+		input.style.height="50px";
+		input.style.width="60%";
+		input.style.fontSize="14pt";
+		input.id = 'ingredient';
+		input.style.marginTop="30px";
+		ingredients_col1_ABD.appendChild(input);
+
+		var list = document.createElement('ul');
+		list.id = 'ingList';
+		list.style.paddingTop="2%";
+		ingredients_col1_ABD.appendChild(list);
+
+		var searchdiv = document.createElement("div");
+		searchdiv.setAttribute("class", "container");
+		searchdiv.style.marginTop="20px";
+		ingredients_col1_ABD.appendChild(searchdiv);
+
+		var button = document.createElement('button');
+		button.setAttribute("class", "searchbutton");
+		button.id = 'addIngredient';
+		button.innerHTML = 'Add Ingredient +';
+		button.onclick = function () {
+			var text = input.value;
+			if (text.length < 1)
+				return;
+			var li = document.createElement('div');
+			list.appendChild(li);
+
+			var label = document.createElement('label');
+			label.innerHTML = text;
+			li.appendChild(label);
+
+			var deleteButton = document.createElement('button');
+			deleteButton.className = 'delete';
+			deleteButton.innerHTML = '-';
+			deleteButton.onclick = function(){
+				list.removeChild(this.parentElement);
+			}
+			li.appendChild(deleteButton);
+			ingredients_search_stage_functions.stageB();
+		};
+		searchdiv.appendChild(button);
+	}
+	ingredients_search_col_1.appendChild(ingredients_col1_ABD);
 }
 
 function ingredients_page_hide() {
